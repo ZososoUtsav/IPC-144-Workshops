33a3ceb9286e73c13482c3452b20e5228083953a
utsav gautam <121043963+UtsavHere@users.noreply.github.com>
2022-12-19 11:17:43 -0500

    Workshop1 files added

---
+++ workshop 1/w1p1.c
@@ -0,0 +1,23 @@
+#include<stdio.h> 
+int main(){
+    //display
+    printf("Workshop 1 Part-1\n");
+    printf("=================\n\n"); 
+    printf("I'm displaying this using the printf\n"); 
+    printf("(standard input output) library function!\n\n");
+    printf("Dear teacher,\n\n"); 
+    printf("\tI promise I will work from this day onward\n"); 
+    printf("\tI acknowledge that practice is extremely improtant,\n"); 
+    printf("\tso I will do all workshops, quizzes, and assignments.\n\n"); 
+    printf("Sincerely,\n\n"); 
+    printf("First_Name Last_Name\n\n"); 
+    printf("Student_ID# 123456789\n"); 
+
+    return 0; // return to the OS that it ran successfully without any error
+     
+
+
+
+
+
+}
